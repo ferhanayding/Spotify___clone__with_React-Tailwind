792f5c98f5086902db1cc7ca305cb80be5c220ba
ferhanayding <ferhanaydin099@gmail.com>
2022-02-07 00:18:48 +0400

    b1 giriş...

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "img/logo.svg";
+const Sidebar = () => {
+  return (
+    <div className="w-60">
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+export default Sidebar;
